e94f25b58046939b6eb7a5311e9f0b725ef95d6b
liuyang1 <liuyang1@mail.ustc.edu.cn>
2018-08-14 11:17:21 +0800

    添加中文文档

--- script/falcon.py
+++ script/falcon.py
@@ -1,19 +1,24 @@
-"""
-Yuan Fei Li Tian Zhe, Wang Feng Xi Xin;
+#! -*- encoding: utf8
+
+"""鸢飞戾天者，望峰息心
 
 python falcon.py [ledger file]
 
 ledger file format:
 
     YYYYMMDD    PROJECT_NAME    [+-]NUMBER
+    日期        项目名称        流入流出金额
 
 It will show result:
 
-    PROJECT_NAME    FINAL_NUMBER    NUMBERxDAY
+    PROJECT_NAME    FINAL_NUMBER    NUMBER*YEAR
+    项目名称        当前累计金额    元*年
 
 It will help to calculate interest in investment.
+    ratio = Interest / SUM1
 
-ratio = Interest / SUM1
+在频繁操作的情况下，这个脚本帮助计算自己的年化收益
+    年化收益 = 收益 / 元年
 """
 
 import sys
@@ -68,15 +73,18 @@ def calc(dat):
 
 
 if __name__ == "__main__":
-    print "Now:", today
+    if len(sys.argv) <= 1:
+        print __doc__
+        sys.exit(-1)
+    print "End Day:", today
     lst = loadData(sys.argv[1])
     # print lst
     dct = splitAsPjt(lst)
-    print '%-20s %10s %20s %20s' % ("PROJECT_NAME", "SUM0/rmb", "SUM1/(rmb*day)", "SUM1(/rmb*year)")
+    print '%-20s %10s %20s' % ("PROJECT_NAME", "SUM0/rmb", "SUM1(/rmb*year)")
     s0, s1 = 0, 0
     for pjt, dat in dct.items():
         r = calc(dat)
         s0 += r[0]
         s1 += r[1]
-        print "%-20s %10.2f %20.2f %20.2f" % (pjt, r[0], r[1], r[1]/365.)
-    print "%-20s %10.2f %20.2f %20.2f" % ('sum', s0, s1, s1/365.)
+        print "%-20s %10.2f %20.2f" % (pjt, r[0], r[1]/365.)
+    print "%-20s %10.2f %20.2f" % ('SUM', s0, s1/365.)
